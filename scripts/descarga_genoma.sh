4a90a3a31a30e4f9eaa7dcf0021a41be8122f4bf
Fernando Barquín <locofoco98@gmail.com>
2023-11-16 19:25:53 +0100

    descarga_genoma

---
+++ scripts/descarga_genoma.sh
@@ -0,0 +1,3 @@
+mkdir -p res/genome
+wget -O res/genome/Genoma1.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+gunzip -k res/genome/Genoma1.fasta.gz
